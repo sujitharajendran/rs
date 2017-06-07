a0c87992ffb4c54c7e9d9bb8c3a5ab355872c953
sujitharajendran <sujikavi66@gmail.com>
2017-06-07 09:54:48 +0530

    Add files via upload

---
+++ odd.java
@@ -0,0 +1,19 @@
+package odd;
+
+public class odd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  int num =3;
+  if(num%2==0)
+  {
+	 System.out.println("the num is even"+num);
+	 
+  }
+  else
+  {
+  System.out.println("the num is odd"+num);
+	}
+
+}
+}
